5620a35e2ba782eac1dd1d94318514205a3425b1
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-29 21:03:25 +0100

    Update login.js
    Added credentials option to allow cookie sharing to other domain.

--- src/api/login.js
+++ src/api/login.js
@@ -5,7 +5,7 @@ const url = process.env.REACT_APP_API_URL;
 export default async function login(usernameEmail, password) {
 
     try {
-        const response = await axios.post(`${url}/api/login`, { usernameEmail, password });
+        const response = await axios.post(`${url}/api/login`, { usernameEmail, password }, {credentials :"include"});
         return response.data;
     } catch (error) {
         const response = error;
