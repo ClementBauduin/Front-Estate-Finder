662b137af4753f58046285debaaea57336404f17
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-29 19:53:56 +0100

    Update login.js
    Made login function async.

--- src/api/login.js
+++ src/api/login.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_API_URL;
 
-export function login(usernameEmail, password) {
-    return axios.post(`${url}/api/login`, { usernameEmail, password })
-        .then(res => res.data)
-        .catch((error) => {
-            console.log(error);
-          });
+export default async function login(usernameEmail, password) {
+
+    try {
+        const response = await axios.post(`${url}/api/login`, { usernameEmail, password });
+        return response.data;
+    } catch (error) {
+        const response = error;
+        return response;
+    }
+
 }
