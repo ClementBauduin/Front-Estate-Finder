8fa6491fd3a996cd16c6823728ec945eb10e61a9
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-30 22:20:43 +0100

    Update getUserEstate.js

--- src/api/getUserEstate.js
+++ src/api/getUserEstate.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getUserEstate(username) {
     try {
-        const response = await axios.get(`/api/userestates/${username}`);
+        const response = await axios.get(`/api/userestates/${username}`, {withCredentials: true});
         return response.data;
     } catch (error) {
         console.log(error);
