6ee251cfd9d360723a6d709a5317ed612bd1ea53
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-30 22:21:05 +0100

    Update deleteEstate.js

--- src/api/deleteEstate.js
+++ src/api/deleteEstate.js
@@ -4,7 +4,7 @@ export default async function deleteEstate(id) {
     const url = process.env.REACT_APP_API_URL;
     
     try {
-        const response = await axios.delete(`${url}/api/deleteEstate/${id}`);
+        const response = await axios.delete(`${url}/api/deleteEstate/${id}`, {withCredentials: true});
         return response.data;
     } catch (error) {
         console.error(error);
