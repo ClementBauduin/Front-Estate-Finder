6996773197741025239addf61cb6562fee8bafa0
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-30 22:21:14 +0100

    Update editEstate.js

--- src/api/editEstate.js
+++ src/api/editEstate.js
@@ -4,7 +4,7 @@ const url = process.env.REACT_APP_API_URL;
 export default async function editEstate(id,editedEstate) {
     
     try {
-        const response = await axios.patch(`${url}/api/editEstate/${id}`,editedEstate)
+        const response = await axios.patch(`${url}/api/editEstate/${id}`,editedEstate, {withCredentials: true})
         return (response.data);
     } catch (error) {
         console.error(error);
