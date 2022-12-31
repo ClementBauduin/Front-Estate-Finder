2bc36d5e5bc328b0be86016f96f5411d60d72463
ClementBauduin <103571576+ClementBauduin@users.noreply.github.com>
2022-12-31 18:01:41 +0100

    Update logout.js

--- src/api/logout.js
+++ src/api/logout.js
@@ -3,6 +3,10 @@ import axios from "axios"
 const url = process.env.REACT_APP_API_URL;
 
 export function logout () {
-    axios.get(`${url}/api/logout`, {withCredentials :"include"})
-    
+
+    try {
+        axios.get(`${url}/api/logout`, {withCredentials :"include"});
+    } catch (error) {
+        console.log(error)
+    }
 }
